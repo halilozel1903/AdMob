ec0818bc20b55d74ab42807aa6cf03d19b27b2bb
Halil Özel <halilozel1903@gmail.com>
2018-09-11 00:09:06 +0300

    proje dosyaları eklendi.

---
+++ AdmobApp/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  AdmobApp
+//
+//  Created by Halil Özel on 10.09.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class ViewController: UIViewController,GADInterstitialDelegate {
+
+    var interstitial: GADInterstitial!
+    
+    @IBOutlet weak var bannerView: GADBannerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // banner reklamı
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+        
+        // gecis reklamı
+         interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        interstitial.delegate = self
+        let request = GADRequest()
+        interstitial.load(request)
+    }
+
+   
+    @IBAction func showshowInterstitial(_ sender: Any) {
+        
+        if interstitial.isReady{
+            interstitial.present(fromRootViewController: self)
+        }else{
+            print("Reklam hazır değil")
+        }
+    }
+    
+    
+    /// Tells the delegate an ad request succeeded.
+    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+        print("interstitialDidReceiveAd")
+    
+    }
+    
+    /// Tells the delegate an ad request failed.
+    func interstitial(_ ad: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
+        print("interstitial:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+    
+    /// Tells the delegate that an interstitial will be presented.
+    func interstitialWillPresentScreen(_ ad: GADInterstitial) {
+        print("interstitialWillPresentScreen")
+    }
+    
+    /// Tells the delegate the interstitial is to be animated off the screen.
+    func interstitialWillDismissScreen(_ ad: GADInterstitial) {
+        print("interstitialWillDismissScreen")
+    }
+    
+    /// Tells the delegate the interstitial had been animated off the screen.
+    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+        print("interstitialDidDismissScreen")
+    }
+    
+    /// Tells the delegate that a user click will open another app
+    /// (such as the App Store), backgrounding the current app.
+    func interstitialWillLeaveApplication(_ ad: GADInterstitial) {
+        print("interstitialWillLeaveApplication")
+    }
+
+    
+}
+
